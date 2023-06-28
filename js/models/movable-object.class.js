8f3d94dcf3366cfa449314d285a80c4482c54c7f
Programmika <rothe.annika@yahoo.com>
2023-06-28 11:13:58 +0200

    pepe dies

--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -10,7 +10,8 @@ class MovableObject {
   otherDirection = false;
   speedY = 0;
   acceleration = 2;
-  energy= 100;
+  energy = 100;
+  lastHit = 0;
 
   applyGravity() {
     setInterval(() => {
@@ -31,28 +32,48 @@ class MovableObject {
     this.img.src = path;
   }
 
-  
   draw(ctx) {
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
 
   drawFrame(ctx) {
-
-    if (this instanceof Character || this instanceof Chicken){
-    ctx.beginPath();
-    ctx.lineWidth = "7";
-    ctx.strokeStyle = "green";
-    ctx.rect(this.x, this.y, this.width, this.height);
-    ctx.stroke();
+    if (this instanceof Character || this instanceof Chicken) {
+      ctx.beginPath();
+      ctx.lineWidth = "7";
+      ctx.strokeStyle = "green";
+      ctx.rect(this.x, this.y, this.width, this.height);
+      ctx.stroke();
     }
   }
 
-  isColliding (mo) {
-    return  this.x + this.width > mo.x &&
-            this.y + this.height > mo.y &&
-            this.x < mo.x &&
-            this.y < mo.y + mo.height;
+  isColliding(mo) {
+    return (
+      this.x + this.width > mo.x &&
+      this.y + this.height > mo.y &&
+      this.x < mo.x &&
+      this.y < mo.y + mo.height
+    );
   }
+
+  hit() {
+    this.energy -= 5;
+    if (this.energy < 0) {
+      this.energy = 0;
+    } else {
+      this.lastHit = new Date().getTime();
+    }
+  }
+
+  isHurt() {
+    let timePassed = new Date().getTime() - this.lastHit; //Differenz in Millisekunden
+    timePassed = timePassed / 1000; // Differenz in Sekunden
+    return timePassed < 1; //wenn Pepe innerhalb der letzten 1 Sekunden getroffen wurde, dann true (Animation wird abgespielt)
+  }
+
+  isDead() {
+    return this.energy == 0;
+  }
+
   /**
    *
    * @param {Array} arr - ['img/image1.png, img/image2.png,....]
@@ -66,7 +87,7 @@ class MovableObject {
   }
 
   playAnimation(images) {
-    let i = this.currentImage % this.IMAGES_WALKING.length; //let i = 0 % 6 (Modulu % ist der mathematische Rest)
+    let i = this.currentImage % images.length; //let i = 0 % 6 (Modulu % ist der mathematische Rest)
     // i = 0,1,2,3,4,5,0,1,2,3,4,5,0....
     let path = images[i];
     this.img = this.imageCache[path];
